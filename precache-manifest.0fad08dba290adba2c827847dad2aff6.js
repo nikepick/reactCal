4b9429e2b0794b96a71739cd43d6ccc8a12d9cd0
“nikhilyadav” <nikhil.yadav@obcc.de>
2020-09-16 13:20:48 +0200

    Updates

---
+++ precache-manifest.0fad08dba290adba2c827847dad2aff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1439c59fc4844470e47209d9155d3851",
+    "url": "/reactCal/index.html"
+  },
+  {
+    "revision": "998d87fe3884beb2f047",
+    "url": "/reactCal/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "138710a8d85a6c5179d9",
+    "url": "/reactCal/static/js/2.823bbf86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactCal/static/js/2.823bbf86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998d87fe3884beb2f047",
+    "url": "/reactCal/static/js/main.a8572d5f.chunk.js"
+  },
+  {
+    "revision": "6993ff9fd5c583c1265c",
+    "url": "/reactCal/static/js/runtime-main.fbf80413.js"
+  }
+]);
